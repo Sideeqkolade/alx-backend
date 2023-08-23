035b2f8bbdd8fade9feaf9b595ad5616dd53d923
Sideeqkolade <sideeqkolade@gmail.com>
2023-08-24 00:03:12 +0100

    task 1 completed

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" A FIFOCache module """
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class FIFOCache(BaseCaching):
+    """ class inherits from BaseCatching and is a caching system """
+    def __init__(self):
+        """ initialize cache """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add an item """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {first_key}")
+
+    def get(self, key):
+        """ get an item """
+        return self.cache_data.get(key, None)
