63d7627bf7c1eebc6c25aa9251164989e69279bf
Sideeqkolade <sideeqkolade@gmail.com>
2023-08-30 00:55:48 +0100

    task 2 completed

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -26,7 +26,7 @@ def get_locale() -> str:
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """returns the index.html page"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
