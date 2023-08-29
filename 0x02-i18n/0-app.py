3d4a591e91cc43a9333554d5d7c5258df7cdc6bc
Sideeqkolade <sideeqkolade@gmail.com>
2023-08-30 00:14:35 +0100

    task 1 completed

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,4 +10,5 @@ def show_page()-> str:
     return render_template('0-index.html')
 
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""A flask app and babel class"""
+from flask_babel import Babel
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """Config class for babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """returns the index.html page"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
