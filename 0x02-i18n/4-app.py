2fd8364ebf15259622c7b9e7b984a0b560243a19
Sideeqkolade <sideeqkolade@gmail.com>
2023-08-30 17:56:16 +0100

    task 4 completed

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""A flask app and babel class"""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+class Config:
+    """Config class for babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_locale() -> str:
+    """Translates to the best match language"""
+    locale = request.args.get('locale')
+
+    # Check if the 'locale' query parameter is present in the request
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """returns the index.html page"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
