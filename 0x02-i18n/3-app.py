fd27e8452a8adebe526f95e35a77b5833fffabbd
Sideeqkolade <sideeqkolade@gmail.com>
2023-08-30 18:11:26 +0100

    task 3 updTED

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -16,12 +16,13 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale() -> str:
     """Translates to the best match language"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
+# babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/', strict_slashes=False)
